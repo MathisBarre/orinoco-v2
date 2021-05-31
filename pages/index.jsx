5a4b0a69d0e2ec11404646c5d3d4e5401f75fbbf
Mathis Barré <mathis.barre@live.fr>
2021-05-31 11:46:08 +0200

    feat: index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,14 @@ export default function Home () {
         title="Accueil - Orinoco, votre e-commerce ! Un projet OpenClassrooms"
         description="Le site e-commerce pour vos oursons en peluche ! Un projet OpenClassrooms réalisé par Mathis Barré"
       />
-      <h1>Orinoco</h1>
+      <ul id="productsList" className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+        <div className="bg-oniPink animate-pulse h-80 rounded-md" />
+      </ul>
     </>
   )
 }
