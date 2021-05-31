11e741b005491953f6bd7da3bbb3f8df9155c2e3
Mathis Barré <mathis.barre@live.fr>
2021-05-31 11:45:50 +0200

    feat: import config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        oniPink: '#f3e9f1'
+      },
+      scale: {
+        102: '1.02'
+      },
+      minWidth: {
+        '1/2': '50%'
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')]
 }
