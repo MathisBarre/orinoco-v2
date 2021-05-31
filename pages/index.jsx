108ca742a242c0b8b1d7e1f74693a8cb6c70e5ff
Mathis Barré <mathis.barre@live.fr>
2021-05-31 11:14:23 +0200

    feat: project init

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Accueil - Orinoco</title>
+      </Head>
+      <h1>Orinoco</h1>
+    </>
+  )
+}
