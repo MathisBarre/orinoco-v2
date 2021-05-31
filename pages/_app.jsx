a96eed811b54598785395ca9d94c7119d396ac71
Mathis Barré <mathis.barre@live.fr>
2021-05-31 11:46:00 +0200

    feat: header

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,7 +11,20 @@ function MyApp ({ Component, pageProps }) {
         title="Orinoco, votre e-commerce ! Un projet OpenClassrooms"
         description="Le site e-commerce pour vos oursons en peluche ! Un projet OpenClassrooms réalisé par Mathis Barré"
       />
-      <Component {...pageProps} />
+      <div className="container mx-auto px-4 py-8">
+        <header className="flex justify-between items-center rounded-lg w-full bg-oniPink py-2 px-6">
+          <a href="/"><img className="h-20 bg-oniPink" src="./images/logo.png" alt="Orinico" /></a>
+          <nav className="flex">
+            <ul className="list-none font-semibold">
+              <li>
+                <a className="px-4 py-4" href="./cart.html">Panier</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Component {...pageProps} />
+      </div>
+
     </>
   )
 }
