4227ef61f641ba100d33a92ee6f25a5d5d6b20f6
Mathis Barré <mathis.barre@live.fr>
2021-09-09 17:21:01 +0200

    feat: add devs.coffee group banner

---
+++ components/DevsCoffeeBanner.jsx
@@ -0,0 +1,24 @@
+export default function DevsCoffeeBanner () {
+  return (
+    <section style={{ backgroundColor: '#1F2937', color: 'white', fontSize: 14, padding: '8px 16px', fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', position: 'sticky', top: 0, zIndex: 999999, overflow: 'hidden' }}>
+      <div className="container px-4 mx-auto" style={{ margin: 'auto', display: 'flex', justifyContent: 'space-between', position: 'relative' }}>
+        <img style={{ position: 'absolute', height: 38 }} src="https://devs.coffee/images/logo.svg" alt="devs.coffee logo" />
+        <div style={{ marginLeft: '3.5rem' }}>
+          Un projet{' '}
+          <a href="https://devs.coffee" style={{ color: 'white', fontWeight: 'bolder', textDecoration: 'underline' }}>devs.coffee</a>{' '}
+          <span style={{ fontSize: '0.75rem', opacity: '.90' }}>
+            par
+            <a href="https://mathisbarre.com" style={{ color: 'inherit', fontWeight: 'bolder', textDecoration: 'none' }}>{' '}Mathis Barré</a>
+          </span>
+        </div>
+        <nav className="hidden sm:block">
+          <a target="_blank" className="hover:underline" href="https://reservia.mathisbarre.com" rel="noreferrer">Reservia</a>
+          <span className="cursor-default">{' '}-{' '}</span>
+          <a target="_blank" className="hover:underline" href="https://ohmyfood.mathisbarre.com" rel="noreferrer">OhMyFood</a>
+          <span className="cursor-default">{' '}-{' '}</span>
+          <a target="_blank" className="hover:underline" href="https://orinoco-v2.mathisbarre.com" rel="noreferrer">Orinoco</a>
+        </nav>
+      </div>
+    </section>
+  )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import splitbee from '@splitbee/web'
 import Seo from '../components/Seo'
+import DevsCoffeeBanner from '../components/DevsCoffeeBanner'
 
 function MyApp ({ Component, pageProps }) {
   useEffect(() => {
@@ -22,6 +23,7 @@ function MyApp ({ Component, pageProps }) {
         description="Le site e-commerce pour vos oursons en peluche ! Projet 5 du parcours développeur web chez OpenClassrooms réalisé par Mathis Barré"
         imageUrl="https://orinoco-v2.mathisbarre.com/images/orinoco.png"
       />
+      <DevsCoffeeBanner />
       <div className="container px-4 py-8 mx-auto">
         <header className="flex items-center justify-between w-full px-6 py-2 rounded-lg bg-oniPink">
           <Link href="/"><a><img className="h-20 bg-oniPink" src="/images/logo.png" alt="Orinico" /></a></Link>
