c6ff646a2f1ce6c29c05d0a860158560fb80a672
Mathis Barré <mathis.barre@live.fr>
2021-09-09 16:56:01 +0200

    feat: add splitbee analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,19 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import splitbee from '@splitbee/web'
 import Seo from '../components/Seo'
 
 function MyApp ({ Component, pageProps }) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: '/bee.js',
+      apiUrl: '/_hive'
+    })
+  }, [])
+
   return (
     <>
       <Head><link rel="icon" href="/favicon.ico" /></Head>
@@ -13,11 +22,11 @@ function MyApp ({ Component, pageProps }) {
         description="Le site e-commerce pour vos oursons en peluche ! Projet 5 du parcours développeur web chez OpenClassrooms réalisé par Mathis Barré"
         imageUrl="https://orinoco-v2.mathisbarre.com/images/orinoco.png"
       />
-      <div className="container mx-auto px-4 py-8">
-        <header className="flex justify-between items-center rounded-lg w-full bg-oniPink py-2 px-6">
+      <div className="container px-4 py-8 mx-auto">
+        <header className="flex items-center justify-between w-full px-6 py-2 rounded-lg bg-oniPink">
           <Link href="/"><a><img className="h-20 bg-oniPink" src="/images/logo.png" alt="Orinico" /></a></Link>
           <nav className="flex">
-            <ul className="list-none font-semibold">
+            <ul className="font-semibold list-none">
               <li>
                 <Link href="/cart" ><a className="px-4 py-4">Panier</a></Link>
               </li>
